2d21e594944e1995dcb460712688924bdf1a3b1e
james <arvinjamesinanoria@gmail.com>
2022-10-02 22:56:05 -0700

    Added a options lists and popup window prompts to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,53 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword () {
+
+  var userInput = window.prompt("How long do you want your password to be?")
+
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength)) {
+    window.alert("That's not a number!")
+    return 
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be between 8 and 128 characters.")
+    return
+  }
+ 
+  var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
+  var userWantsSymbols = window.confirm("Would you like to include symbols in your password?")
+  var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+  var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
+
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] 
+  var symbolList = ["!", "@", "#", "$", "%", "^", "&","*", "(", ")", "-", "_", "=", "+", "<", ">", "/", "?", "{", "}", "[", "]"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+  var optionsList = []
+
+  if (userWantsNumbers === true) {
+    optionsList.push(numberList)
+  }
+
+  if (userWantsSymbols === true) {
+    optionsList.push(symbolList)
+  }
+  if (userWantsLowercase=== true) {
+    optionsList.push(lowercaseList)
+  }
+
+  if (userWantsUppercase === true) {
+    optionsList.push(uppercaseList)
+  }
+
+
+
+
+}
 
 // Write password to the #password input
 function writePassword() {
